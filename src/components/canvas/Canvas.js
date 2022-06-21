b90777113f74f27ca6a988c997495b9766beea3f
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-06-21 21:37:40 +0200

    WIP, Canvas logic

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,42 +1,25 @@
 import './canvas.css';
-import { useRef, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
+// import { getCommandObject } from '../../lib';
+// import Robot from '../robot/Robot';
 
-const Canvas = ({ cmd = null }) => {
+const Canvas = ({ command, draw }) => {
   const canvasref = useRef(null);
-  //  the canvas receives the commad from the app/commandcenter
-  // the canvas executes the commands
-  // the canvas redraws the robot in the new position
-  // the canvas keeps record of the previous position of the robot
-
-  /**
-   * COMMANDS (convention over code)
-   * PLACE x,y, Orientation
-   * REPORT - reports the current location
-   * MOVE - moves Robot in the orientation direction
-   * LEFT - rotates the element 90deg to the left
-   * RIGHT - rotates the element/robot 90deg to the right.
-   */
+  const [ctx, setCtx] = useState(null);
+  // const [robot, setRobot] = useState(null);
 
   useEffect(() => {
     const canvas = canvasref.current;
     if (!canvas) return;
-    const ctx = canvas.getContext('2d');
-    const animateItems = () => {
-      window.requestAnimationFrame(animateItems);
-    };
-    animateItems();
-    // robot color
 
-    // we need to find sprites for the robot here.
-    ctx.fillStyle = 'red';
-    ctx.fillRect(0, 0, 50, 50);
-  }, [cmd]);
+    const context = canvas.getContext('2d');
+    if (!context) return;
+    setCtx(context);
+    draw(ctx);
+  }, [draw, ctx]);
 
-  // const _Move = () => {}
-  // const _RIGHT = () => {}
-  // const _LEFT = () => {}
-  // const _REPORT = () => {}
+  console.log('cmd', command);
 
-  return <canvas id='terrain' ref={canvasref}></canvas>;
+  return <canvas id='terrain' ref={canvasref} />;
 };
 export default Canvas;
