56bf0f102b3b2fc2294f088b648e6bf631912e9b
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-06-21 11:16:16 +0200

    added styled component dependancy to applucation, created basic layout of application

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,46 @@
+import { useEffect, useState, useRef } from 'react';
+import styled from 'styled-components';
+import Canvas from '../canvas/Canvas';
+import './app.css';
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  flex-shrink: 1;
+  position: relative;
+`;
+
+const CommandCenter = styled.section`
+  height: 100%;
+  width: 20%;
+  min-width: 250px;
+`;
+
+const Terrain = styled.section`
+  flex: 1;
+`;
+
 const App = () => {
-  return <p>Hello I am an app</p>;
+  const [test, setTest] = useState(null);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    setTest('fuck you anny');
+    if (test) {
+      console.log(test);
+    }
+  }, [test, ref]);
+
+  return (
+    <AppWrapper>
+      <Terrain>
+        <Canvas ref={ref} />
+      </Terrain>
+      <CommandCenter></CommandCenter>
+    </AppWrapper>
+  );
 };
 
 export default App;
---
+++ src/components/canvas/Canvas.js
@@ -0,0 +1,6 @@
+import './canvas.css';
+
+const Canvas = () => {
+  return <canvas id='terrain'></canvas>;
+};
+export default Canvas;
