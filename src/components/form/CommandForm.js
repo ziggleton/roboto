7159b01cab8ec04f166fecd828761be485e9fc06
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-06-22 21:55:40 +0200

    command form needed slight refactoring to include validation

--- src/components/form/CommandForm.js
+++ src/components/form/CommandForm.js
@@ -21,6 +21,7 @@ const Button = styled.button`
 const CommandForm = ({ callback }) => {
   const formRef = useRef(null);
   const [cmdInput, setCmdInput] = useState(null);
+  const [disabled, setDisabled] = useState('disabled');
 
   useEffect(() => {
     const frm = formRef.current;
@@ -28,20 +29,28 @@ const CommandForm = ({ callback }) => {
     setCmdInput(frm.querySelector('input'));
   }, []);
 
+  const toggleInput = () => {
+    setDisabled(
+      cmdInput.value === '' || cmdInput.value === null ? 'disabled' : ''
+    );
+  };
+
   const acceptValue = (event) => {
     event.preventDefault();
     if (!cmdInput) return;
-    callback(cmdInput.value.trim());
+    callback(cmdInput.value.trim().toUpperCase());
     setTimeout(() => {
-      cmdInput.value = null;
       callback('STOP');
-    }, 100);
+      cmdInput.value = null;
+      toggleInput();
+      cmdInput.focus();
+    }, 10);
   };
 
   return (
     <form ref={formRef} onSubmit={acceptValue}>
-      <Input type='text' autoFocus={true} />
-      <Button name='button'>execute command</Button>
+      <Input type='text' autoFocus={true} onChange={toggleInput} />
+      <Button disabled={disabled}>execute command</Button>
     </form>
   );
 };
