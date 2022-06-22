410c61c40504821c7a70130c39ef8eb74351d277
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-06-22 21:46:04 +0200

    updated error heandling for the helper and extended the regex

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,7 @@
 const isPlace = new RegExp(
-  /^(PLACE) ([0-9]{1,2}),([0-9]{1,2}),(NORTH|SOUTH|EAST|WEST)/
+  /^(PLACE) ([0-9]{1,3}),([0-9]{1,3}),(NORTH|SOUTH|EAST|WEST)/
 );
-const actionType = new RegExp(/^(LEFT|RIGHT|REPORT|MOVE)/);
+const actionType = new RegExp(/^(LEFT|RIGHT|REPORT|MOVE|STOP)/);
 
 const getCommandObject = (cmdString) => {
   let obj = {
@@ -10,41 +10,51 @@ const getCommandObject = (cmdString) => {
       x: null,
       y: null,
     },
-    orientation: null,
+    orientation: {
+      deg: null,
+      bearing: null,
+    },
   };
   if (isPlace.test(cmdString)) {
     const primaryArr = cmdString.split(' ');
+
     if (primaryArr.length) {
       obj.trigger = primaryArr[0];
     }
+
     if (primaryArr[1].length) {
       const secondaryArr = primaryArr[1].split(',');
-      obj.position.x = secondaryArr[0];
-      obj.position.y = secondaryArr[1];
+      obj.position.x = parseInt(secondaryArr[0]);
+      obj.position.y = parseInt(secondaryArr[1]);
       switch (secondaryArr[2]) {
         case 'NORTH':
-          obj.orientation = 0;
+          obj.orientation.deg = 360;
+          obj.orientation.bearing = secondaryArr[2];
           break;
         case 'SOUTH':
-          obj.orientation = 180;
+          obj.orientation.deg = 180;
+          obj.orientation.bearing = secondaryArr[2];
           break;
         case 'EAST':
-          obj.orientation = 270;
+          obj.orientation.deg = 90;
+          obj.orientation.bearing = secondaryArr[2];
           break;
         case 'WEST':
-          obj.orientation = 90;
+          obj.orientation.deg = 270;
+          obj.orientation.bearing = secondaryArr[2];
           break;
         default:
-          obj.orientation = 0;
+          obj.orientation.deg = 360;
+          obj.orientation.bearing = 'NORTH';
           break;
       }
     }
   } else if (actionType.test(cmdString)) {
+    obj.trigger = cmdString;
     delete obj.position;
     delete obj.orientation;
-    obj.trigger = cmdString;
   } else {
-    obj = new Error(cmdString);
+    obj = null;
   }
 
   return obj;
