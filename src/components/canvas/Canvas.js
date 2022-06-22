9410bc4b97d7fb3e75c8c33cad8256aa46e428ab
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-06-22 21:53:01 +0200

    updated canvas code to work on load and to check the existance of the robot

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,24 +1,39 @@
 import './canvas.css';
-import { useRef, useState, useEffect } from 'react';
-// import { getCommandObject } from '../../lib';
-// import Robot from '../robot/Robot';
+import Robot from '../robot/Robot';
+import { getCommandObject } from '../../lib';
+import { useRef, useEffect } from 'react';
 
 const Canvas = ({ command, draw }) => {
   const canvasref = useRef(null);
-  const [ctx, setCtx] = useState(null);
-  // const [robot, setRobot] = useState(null);
+  const robot = useRef(null);
+  const context = useRef(null);
+  const cmd = useRef(null);
 
   useEffect(() => {
+    // get canvas context
     const canvas = canvasref.current;
-    if (!canvas) return;
+    context.value = canvas.getContext('2d');
+    // draw the canvas on the screen
+    if (!context.value) return;
+    draw(context.value);
+    // get commandObject from the command
+    if (!command) return;
+    cmd.value = getCommandObject(command);
+    if (!cmd.value) return;
+    if (cmd.value.trigger === 'PLACE' && !robot.value) {
+      robot.value = new Robot({ context: context.value, cmd: cmd.value });
+    }
+    if (!robot.value) return;
+    robot.value.command = { ...cmd.value };
+    const animateRobot = () => {
+      window.requestAnimationFrame(animateRobot);
+      context.value.fillStyle = 'black';
+      context.value.fillRect(0, 0, canvas.width, canvas.height);
+      robot.value._UPDATE();
+    };
 
-    const context = canvas.getContext('2d');
-    if (!context) return;
-    setCtx(context);
-    draw(ctx);
-  }, [draw, ctx]);
-
-  console.log('cmd', command);
+    animateRobot();
+  }, [draw, command]);
 
   return <canvas id='terrain' ref={canvasref} />;
 };
